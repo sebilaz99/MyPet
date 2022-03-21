a105248d7de817f8885165fbe28b259dd40bbaeb
DaLazea <danielsebastian.lazea@endava.com>
2022-03-21 17:39:50 +0200

    Finished all 3 registration fragments - no firebase yet

--- app/src/main/java/com/example/mypet/registration/RegisterFragment1.kt
+++ app/src/main/java/com/example/mypet/registration/RegisterFragment1.kt
@@ -1,16 +1,50 @@
 package com.example.mypet.registration
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.mypet.R
+import com.example.mypet.model.Species
 
 class RegisterFragment1 : Fragment(R.layout.fragment_register1) {
 
+    lateinit var adapter: ArrayAdapter<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val goToReg2Btn = view.findViewById<AppCompatButton>(R.id.nextRegButton)
+        val petName = view.findViewById<EditText>(R.id.nameEditText)
+        val petBreed = view.findViewById<EditText>(R.id.breedEditText)
+        val sactv = view.findViewById<AutoCompleteTextView>(R.id.speciesAutoCompleteTextView)
+
+        val speciesList = mutableListOf<String>()
+        speciesList.add(Species.Bird.toString())
+        speciesList.add(Species.Cat.toString())
+        speciesList.add(Species.Dog.toString())
+        speciesList.add(Species.Exotic.toString())
+        speciesList.add(Species.Fish.toString())
+        speciesList.add(Species.Rodent.toString())
+
+        adapter = ArrayAdapter(requireContext(), R.layout.species_dropdown_item, speciesList)
+        sactv.setAdapter(adapter)
+
+
+        goToReg2Btn.setOnClickListener {
+            val name = petName.text.toString()
+            val breed = petBreed.text.toString()
+            val species = sactv.text.toString()
+            val action = RegisterFragment1Directions.actionRegisterFragment1ToRegisterFragment2(
+                name,
+                breed,
+                species
+            )
+            view.findNavController().navigate(action)
+        }
     }
 }
